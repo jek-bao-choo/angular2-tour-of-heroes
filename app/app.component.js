bece45c29f115e7e48826022a8c5785aef44b630
Jek Bao CHOO <jerchoo@outlook.com>
2016-01-09 11:43:13 +0800

    feat: add master-detail tutorial section

--- app/app.component.js
+++ app/app.component.js
@@ -9,7 +9,7 @@ System.register(['angular2/core'], function(exports_1) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
     var core_1;
-    var AppComponent;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
@@ -19,23 +19,35 @@ System.register(['angular2/core'], function(exports_1) {
             AppComponent = (function () {
                 function AppComponent() {
                     this.title = 'Tour of Heroes';
-                    this.hero = {
-                        id: 1,
-                        name: 'Windstorm'
-                    };
+                    this.heroes = HEROES;
                 }
+                AppComponent.prototype.onSelect = function (hero) {
+                    this.selectedHero = hero;
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "\n        <h1> {{ title }} </h1>\n        <h2> {{ hero.name }} details! </h2>\n        <div><label for=\"id\">id: </label> {{hero.id}} </div>\n        <div>\n            <label>name: </label>\n            <div><input [(ngModel)]=\"hero.name\" placeholder=\"name\"></div>\n        </div>\n    "
+                        styles: ["\n        .selected {\n            background-color: #CFD8DC !important;\n            color: white;\n         }\n         .heroes {\n            margin: 0 0 2em 0;\n            list-style-type: none;\n            padding: 0;\n            width: 10em;\n         }\n         .heroes li {\n            cursor: pointer;\n            position: relative;\n            left: 0;\n            background-color: #EEE;\n            margin: .5em;\n            padding: .3em 0em;\n            height: 1.6em;\n            border-radius: 4px;\n         }\n         .heroes li.selected:hover {\n            color: white;\n         }\n         .heroes li:hover {\n            color: #607D8B;\n            background-color: #EEE;\n            left: .1em;\n         }\n         .heroes .text {\n            position: relative;\n            top: -3px;\n         }\n         .heroes . badge {\n            display: inline-block;\n            font-size: small;\n            color: white;\n            padding: 0.8em 0.7em 0em 0.7em;\n            background-color: #607D8B;\n            line-height: 1em;\n            position: relative;\n            left: -1px;\n            top: -4px;\n            height: 1.8em;\n            margin-right: .8em;\n            border-radius: 4px 0px 0px 4px;\n         }\n    "],
+                        template: "\n        <h2>My heroes </h2>\n        <ul class=\"heroes\"> <!--class.selected is Styling the selection-->\n        <li *ngFor=\"#hero of heroes\" [class.selected]=\"hero === selectedHero\" (click)=\"onSelect(hero)\">\n            <!--each hero goes here-->\n            <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n        </li>\n        </ul>\n\n        <br>\n\n        <div *ngIf=\"selectedHero\">\n        <h2>{{selectedHero.name}} details</h2>\n        <div><label for=\"id\"> id: </label> {{selectedHero.id}}</div>\n        <div><label for=\"name\">name:</label>\n            <input type=\"text\" [(ngModel)]=\"selectedHero.name\" placeholder=\"name\">\n        </div>\n        </div>\n    "
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
                 return AppComponent;
             })();
-            exports_1("AppComponent", AppComponent);
+            exports_1("AppComponent", AppComponent); //this act of exporting turns the file into a module. The name of the file (without extension) is usually the name of the module
+            HEROES = [
+                { "id": 11, "name": "Mr. Nice" },
+                { "id": 12, "name": "Narco" },
+                { "id": 13, "name": "Bombasto" },
+                { "id": 14, "name": "Celeritas" },
+                { "id": 15, "name": "Magneta" },
+                { "id": 16, "name": "RubberMan" },
+                { "id": 17, "name": "Dynama" },
+                { "id": 18, "name": "Dr IQ" },
+                { "id": 19, "name": "Magma" },
+                { "id": 20, "name": "Tornado" }
+            ];
         }
     }
 });
-//this act of exporting turns the file into a module. The name of the file (without extension) is usually the name of the module 
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -10,22 +10,94 @@ interface Hero {
 
 @Component({
     selector: 'my-app',
+    styles: [`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+         }
+         .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 10em;
+         }
+         .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0em;
+            height: 1.6em;
+            border-radius: 4px;
+         }
+         .heroes li.selected:hover {
+            color: white;
+         }
+         .heroes li:hover {
+            color: #607D8B;
+            background-color: #EEE;
+            left: .1em;
+         }
+         .heroes .text {
+            position: relative;
+            top: -3px;
+         }
+         .heroes . badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0em 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0px 0px 4px;
+         }
+    `],
     template: `
-        <h1> {{ title }} </h1>
-        <h2> {{ hero.name }} details! </h2>
-        <div><label for="id">id: </label> {{hero.id}} </div>
-        <div>
-            <label>name: </label>
-            <div><input [(ngModel)]="hero.name" placeholder="name"></div>
+        <h2>My heroes </h2>
+        <ul class="heroes"> <!--class.selected is Styling the selection-->
+        <li *ngFor="#hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+            <!--each hero goes here-->
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+        </li>
+        </ul>
+
+        <br>
+
+        <div *ngIf="selectedHero">
+        <h2>{{selectedHero.name}} details</h2>
+        <div><label for="id"> id: </label> {{selectedHero.id}}</div>
+        <div><label for="name">name:</label>
+            <input type="text" [(ngModel)]="selectedHero.name" placeholder="name">
+        </div>
         </div>
     `
 })
+//The parenthesis identify the <li> element’s click event as the target. The expression to the right of the equal sign calls the AppComponent method, onSelect(), passing the local template variable hero as an argument. That’s the same hero variable we defined previously in the ngFor.
 
 export class AppComponent {
     public title = 'Tour of Heroes';
-    public hero:Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
-}
-//this act of exporting turns the file into a module. The name of the file (without extension) is usually the name of the module
+    public heroes = HEROES;
+    public selectedHero: Hero;
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
+}//this act of exporting turns the file into a module. The name of the file (without extension) is usually the name of the module
+
+var HEROES:Hero[] = [
+    {"id": 11, "name": "Mr. Nice"},
+    {"id": 12, "name": "Narco"},
+    {"id": 13, "name": "Bombasto"},
+    {"id": 14, "name": "Celeritas"},
+    {"id": 15, "name": "Magneta"},
+    {"id": 16, "name": "RubberMan"},
+    {"id": 17, "name": "Dynama"},
+    {"id": 18, "name": "Dr IQ"},
+    {"id": 19, "name": "Magma"},
+    {"id": 20, "name": "Tornado"}
+];
